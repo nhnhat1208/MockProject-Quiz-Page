ac4ea3c64435d35998375bbfea4e3892ec2c8e3b
namhuynh1813136 <80080972+namhuynh1813136@users.noreply.github.com>
2021-08-26 12:29:40 +0700

    update addquizcontroller

--- Mockup-Project-Quizz/src/main/java/quizz_mockup_project/spring/controller/AddQuizController.java
+++ Mockup-Project-Quizz/src/main/java/quizz_mockup_project/spring/controller/AddQuizController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +16,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-
+import quizz_mockup_project.spring.bean.Category;
 import quizz_mockup_project.spring.bean.Quiz;
 import quizz_mockup_project.spring.bean.Test;
 import quizz_mockup_project.spring.bean.UserAccount;
-import quizz_mockup_project.spring.utils.AppUtils;
 import quizz_mockup_project.spring.utils.DBUtils;
 
 @Controller
@@ -30,8 +28,14 @@ public class AddQuizController {
 	private DBUtils dao;
 
 	@RequestMapping(value = { "/add_quiz" }, method = RequestMethod.GET)
-	public String loadPage() {
-		return "quiz_add";
+	public String loadPage(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		HttpSession session = request.getSession();
+		Object objUserBean = session.getAttribute("user");
+		if (objUserBean == null) {
+			return "login";
+		} else {
+			return "quiz_add";
+		}
 	}
 
 	@RequestMapping(value = { "/addQuiz" }, method = RequestMethod.POST)
@@ -39,21 +43,41 @@ public class AddQuizController {
 			throws ServletException, IOException {
 
 		String message = null;
-		
-		//String username = AppUtils.getLoginedUser(request.getSession()).getUsername();
 
-		String username = "hhnam";
-		
+		// String username =
+		// AppUtils.getLoginedUser(request.getSession()).getUsername();
+		UserAccount user = (UserAccount) request.getSession().getAttribute("loginedUser");
+		String username = user.getUsername();
+
 		response.setContentType("text/html;charset=UTF-8");
 		String topic = request.getParameter("topic");
 		String name = request.getParameter("name");
 		String list = request.getParameter("quizlist");
 		JSONObject listquiz = new JSONObject(list);
 
-		Test test = new Test(0, topic, name, 1, username);
+		Category category = null;
 
 		try {
+			category = this.dao.findCategory(topic);
+			if (category == null) {
+				category = new Category(0, topic, "", "");
+				this.dao.newCategory(category);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			response.getWriter().write("2");
+			return;
+		}
+
+		Test test = new Test(0, topic, name, category.getId(), username);
+
+		try {
+			if (this.dao.findTest(test) != null) {
+				response.getWriter().write("0"); // Test name exist
+				return;
+			}
 			this.dao.newTest(test);
+			test = this.dao.findTest(test);
 		} catch (SQLException e) {
 			e.printStackTrace();
 			response.getWriter().write("2"); // Unknown ERROR
@@ -62,7 +86,7 @@ public class AddQuizController {
 
 		for (Integer i = 0; i < listquiz.length(); i++) {
 			JSONObject quizObj = listquiz.getJSONObject(i.toString());
-			Quiz quiz = new Quiz(0, 0, quizObj.getString("question"), quizObj.getString("correctAnsw"),
+			Quiz quiz = new Quiz(0, test.getId(), quizObj.getString("question"), quizObj.getString("correctAnsw"),
 					quizObj.getString("incorrectAnsw_1"), quizObj.getString("incorrectAnsw_2"),
 					quizObj.getString("incorrectAnsw_3"));
 
